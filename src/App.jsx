4dcdf1315130f439d28cbd156b5e4feebae3a63c
Bhanu <bhanupattemz@gmail.com>
2025-05-20 17:50:50 +0530

    update App.js

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,12 @@ function App() {
   const [grid, setGrid] = useState([])
   const [refresh, setRefresh] = useState(0)
   const [struct, setStruct] = useState([])
-  const [selected, setSelected] = useState(null)
+  const [preMoves, setPreMoves] = useState([])
   const [solved, setSolved] = useState(false)
   const boxRef = useRef(null)
   const [solution, setSolution] = useState([])
-
+  const [currColor, setCurrColor] = useState("red")
+  const colors = ["red", "lightgreen", "yellow", "blue"]
   const changeColor = (row, col, color) => {
     setGrid(prev => {
       const tempGrid = prev.map(row => row.map(cell => ({ ...cell })))
@@ -47,25 +48,28 @@ function App() {
 
 
     let tempSolution = GeneratePuzzle(n)
-    let structures = [...tempSolution]
-
-    // const revealCount = Math.floor(Math.random() * (structures.length / 3)) + Math.min(4, structures.length / 3);
-    const revealCount=6
-
-    const structureCount = tempSolution.length
-
-    const shuffled = [...Array(structureCount).keys()]
-      .sort(() => Math.random() - 0.5)
-
-    const revealIndices = shuffled.slice(0, revealCount)
-
-    const revealSet = new Set(revealIndices)
-
+    let structures = JSON.parse(JSON.stringify(tempSolution))
+    let colors = ["red", "blue", "lightgreen", "yellow"]
     for (let i = 0; i < structures.length; i++) {
+      const group = structures[i];
+      let iscolored = false
+      for (let [x, y] of structures[i].cells) {
+        if ((x == 0 && y == 0) || (x == 0 && y == n - 1) || (x == n - 1 && y == 0) || (x == n - 1 && y == n - 1)) {
+          iscolored = true
+          break
+        }
+      }
+      if (iscolored) {
+        colors = colors.filter((color) => color != group.backgroundColor)
+        for (let [x, y] of group.cells) {
+          tempGrid[x][y].style.backgroundColor = group.backgroundColor || "white";
+          structures[i].backgroundColor = group.backgroundColor || "white";
+        }
+      }
       for (let j = 0; j < structures[i].cells.length; j++) {
         let x = structures[i].cells[j]
-        structures[i].backgroundColor = revealSet.has(i) ? structures[i].backgroundColor : "white"
-        tempGrid[x[0]][x[1]].style.backgroundColor = structures[i].backgroundColor
+        // structures[i].backgroundColor = revealSet.has(i) ? structures[i].backgroundColor : "white"
+        // tempGrid[x[0]][x[1]].style.backgroundColor = structures[i].backgroundColor
 
         for (let k = j + 1; k < structures[i].cells.length; k++) {
           let y = structures[i].cells[k]
@@ -90,9 +94,19 @@ function App() {
         }
       }
     }
+    for (let color of colors) {
+      for (let i = 0; i < structures.length; i++) {
+        const group = structures[i];
+        if (group.backgroundColor == color) {
+          for (let [x, y] of group.cells) {
+            tempGrid[x][y].style.backgroundColor = group.backgroundColor;
+          }
+          break
+        }
+      }
+    }
     setGrid(tempGrid)
     setStruct(structures)
-    console.log(countValidColorings(structures, n))
     setSolution(tempSolution)
   }, [n, refresh])
 
@@ -100,11 +114,11 @@ function App() {
     if (CheckSolution(grid, struct, n)) {
       setSolved(true)
     }
-    console.log(countValidColorings(struct,n))
+    console.log(countValidColorings(struct, n))
   }, [grid])
   return (
     <>
-      <main className='four-colors-main'>
+      <main className='four-colors-main' style={{ cursor: `url(/${currColor}.png) 2 2,auto` }}>
         <h1 className='four-colors-title'>Four colors</h1>
         {solved && <h2 className='four-colors-solved'>Solved</h2>}
         <div className="four-colors-grid" style={{ gridTemplateRows: `repeat(${n}, 50px)`, gridTemplateColumns: `repeat(${n}, 50px)` }}>
@@ -112,12 +126,15 @@ function App() {
             row.map((item, colIndex) => {
               return (
                 <div
-                  className={`four-colors-cell ${item.style.backgroundColor == "white" ? "four-colors-cell-editable" : "four-colors-cell-fixed"}`}
+                  className={`four-colors-cell`}
                   key={`${rowIndex}-${colIndex}`}
-                  style={{ ...item.style }}
+                  style={{
+                    ...item.style, cursor: (item.style.backgroundColor && item.style.backgroundColor != "white") ? "not-allowed" : `url(/${currColor}.png) 2 2,auto`
+                  }}
                   onClick={() => {
-                    if (item.style.backgroundColor == "white") {
-                      setSelected([rowIndex, colIndex])
+                    if (!item.style.backgroundColor || item.style.backgroundColor == "white") {
+                      setPreMoves(prev => [...prev, [rowIndex, colIndex]])
+                      changeColor(rowIndex, colIndex, currColor)
                     }
                   }}
                 />
@@ -125,13 +142,35 @@ function App() {
             })
           )}
         </div>
-        {selected && <div className='four-colors-palette' ref={boxRef}>
-          <div className='four-colors-option four-colors-red' onClick={() => { changeColor(selected[0], selected[1], "red"); setSelected(null) }}></div>
-          <div className='four-colors-option four-colors-green' onClick={() => { changeColor(selected[0], selected[1], "lightgreen"); setSelected(null) }}></div>
-          <div className='four-colors-option four-colors-blue' onClick={() => { changeColor(selected[0], selected[1], "blue"); setSelected(null) }}></div>
-          <div className='four-colors-option four-colors-yellow' onClick={() => { changeColor(selected[0], selected[1], "yellow"); setSelected(null) }}></div>
-        </div>}
+        <div className='four-colors-palette' ref={boxRef} >
+          <div style={{
+            transform: currColor == "red" ? "scale(1.1)" : 'none',
+            borderColor: currColor == "red" ? "#2c3e50" : "none",
+          }} className='four-colors-option four-colors-red' onClick={() => { setCurrColor("red") }}></div>
+          <div style={{
+            transform: currColor == "lightgreen" ? "scale(1.1)" : 'none',
+            borderColor: currColor == "red" ? "#2c3e50" : "none",
+          }} className='four-colors-option four-colors-green' onClick={() => { setCurrColor('lightgreen') }}></div>
+          <div style={{
+            transform: currColor == "blue" ? "scale(1.1)" : 'none',
+            borderColor: currColor == "red" ? "#2c3e50" : "none",
+          }} className='four-colors-option four-colors-blue' onClick={() => { setCurrColor("blue") }}></div>
+          <div style={{
+            transform: currColor == "yellow" ? "scale(1.1)" : 'none',
+            borderColor: currColor == "red" ? "#2c3e50" : "none",
+          }} className='four-colors-option four-colors-yellow' onClick={() => { setCurrColor("yellow") }}></div>
+        </div>
         <div className='four-colors-controls'>
+          <button className='four-colors-button four-colors-refresh-btn' onClick={() => {
+            if (preMoves && preMoves.length != 0) {
+              setPreMoves(prev => {
+                let temp = JSON.parse(JSON.stringify(prev))
+                let [x, y] = temp.pop()
+                changeColor(x, y, "white")
+                return temp
+              })
+            }
+          }}>undo</button>
           <button className='four-colors-button four-colors-solution-btn' onClick={() => {
             setGrid(prev => {
               const newGrid = prev.map(row => row.map(cell => ({ ...cell })))
